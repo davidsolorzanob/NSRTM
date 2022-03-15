9a40bbcfd5f91b632549d19e64498e8e3aabad5e
davidsolorzanob <davidsnet@hotmail.com>
2022-03-15 12:01:09 -0500

    Carga inicial del NSRTM

---
+++ src/app/mock-api/common/navigation/data.ts
@@ -0,0 +1,44 @@
+/* tslint:disable:max-line-length */
+import { FuseNavigationItem } from '@fuse/components/navigation';
+
+export const defaultNavigation: FuseNavigationItem[] = [
+    
+    {
+        id   : 'contribuyente',
+        title: 'Contribuyente',
+        type : 'basic',
+        icon : 'heroicons_outline:chart-pie',
+        link : '/Contribuyente'
+    }
+];
+export const compactNavigation: FuseNavigationItem[] = [
+   
+    {
+        id   : 'contribuyente',
+        title: 'Contribuyente',
+        type : 'basic',
+        icon : 'heroicons_outline:chart-pie',
+        link : '/Contribuyente'
+    }
+
+];
+export const futuristicNavigation: FuseNavigationItem[] = [
+    
+    {
+        id   : 'contribuyente',
+        title: 'Contribuyente',
+        type : 'basic',
+        icon : 'heroicons_outline:chart-pie',
+        link : '/Contribuyente'
+    }
+];
+export const horizontalNavigation: FuseNavigationItem[] = [
+  
+    {
+        id   : 'contribuyente',
+        title: 'Contribuyente',
+        type : 'basic',
+        icon : 'heroicons_outline:chart-pie',
+        link : '/Contribuyente'
+    }
+];
---
+++ src/app/modules/admin/contribuyente/contribuyente.component.ts
@@ -0,0 +1,16 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-contribuyente',
+  templateUrl: './contribuyente.component.html',
+  encapsulation: ViewEncapsulation.None
+})
+export class ContribuyenteComponent {
+
+  constructor() { }
+
+
+
+}
+
+
