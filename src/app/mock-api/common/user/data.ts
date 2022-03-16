af36be1c6fdff256c633fb3f67bd74a8d449bc72
davidsolorzanob <davidsnet@hotmail.com>
2022-03-16 09:38:34 -0500

    Cambio de imagen de avatar

--- src/app/mock-api/common/user/data.ts
+++ src/app/mock-api/common/user/data.ts
@@ -3,6 +3,6 @@ export const user = {
     id    : 'cfaad35d-07a3-4447-a6c3-d8c3d54fd5df',
     name  : 'Administrador MEF',
     email : 'admin@mef.gob.pe',
-    avatar: 'assets/images/avatars/brian-hughes.jpg',
+    avatar: 'assets/images/avatars/foto-david.png',
     status: 'online'
 };
