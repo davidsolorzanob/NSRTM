0a67386ea60501756509a904ca0152b8d8570204
davidsolorzanob <davidsnet@hotmail.com>
2022-06-15 09:58:37 -0500

    dataepicker

---
+++ src/app/models/condicion.models.ts
@@ -0,0 +1,12 @@
+export class Condicion{
+    condicionContribuyente:number;
+    descricionDocumento: string;
+    nroResolucion: string;
+    fechaDocumento: Date;
+    fechaInicio: Date;
+    expedienteAdministrativo: string;
+    fechaExpediente: Date;
+    remuneracionMensual: number;
+    resolucionAlcadia: string;
+    fechaResolucion: Date; 
+}
--- src/app/modules/admin/contribuyente/contribuyente.module.ts
+++ src/app/modules/admin/contribuyente/contribuyente.module.ts
@@ -18,6 +18,8 @@ import { ListComponent } from './list.component';
 import { contribuyenteRoutes} from 'app/modules/admin/contribuyente/contribuyente.routing';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { MatExpansionModule } from '@angular/material/expansion';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
 
 
 
@@ -42,7 +44,9 @@ import { MatExpansionModule } from '@angular/material/expansion';
     FuseAlertModule,
     MatPaginatorModule,
     MatAutocompleteModule,
-    MatExpansionModule
+    MatExpansionModule,
+    MatDatepickerModule,
+    MatNativeDateModule
   ]
 })
 export class ContribuyenteModule { }
